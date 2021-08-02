aa102318d439bb7c34c13b758764e586037e1914
Katie Pustolski <pustolskik@gmail.com>
2021-08-01 17:40:00 -0700

    Added TMPro essentials. Added three new sprites. Includes some UIElement generated files.
    * The UI is not working properly when I want to 9-slice the sprites. It may require a restart of Unity.

---
+++ ModalLayeringSample/Assets/_Scripts/AppManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace CodeSampleModalLayer
+{
+    public class AppManager : MonoBehaviour
+    {
+        [Header("Views")][Space(5)]
+        [SerializeField]
+        private HomeView homeView = default;
+
+        [Header("Prefabs")][Space(5)]
+        // [SerializeField]
+        // private 
+        // Global Static Variable
+        public static AppManager Instance = {get; private set;}
+
+        // App Starts here. Ie. the "main" function
+        void Start()
+        {
+            Instance = this;
+            homeView.Setup();
+        }
+    }
+}
---
+++ ModalLayeringSample/Assets/_Scripts/HomeView.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CodeSampleModalLayer
+{
+    public class HomeView : MonoBehaviour
+    {
+        [SerializeField]
+        private Button button;
+
+
+        public void Setup()
+        {
+            // Set up this view
+        }
+
+        public void Shutdown()
+        {
+            // shutdown this view
+        }
+    }
+}
