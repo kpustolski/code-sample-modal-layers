3ef0c3e1c9342132e227033426bf168f8721b721
Katie Pustolski <pustolskik@gmail.com>
2021-08-03 20:39:35 -0700

    Added a way to pick which direction the background should scroll

--- ModalLayeringSample/Assets/_Scripts/ScrollingBackground.cs
+++ ModalLayeringSample/Assets/_Scripts/ScrollingBackground.cs
@@ -8,9 +8,25 @@ namespace CodeSampleModalLayer
 {
     public class ScrollingBackground : MonoBehaviour
     {
+        public enum ScrollDirection
+        {
+            None = 0,
+            Left = 1,
+            Right = 2,
+            Up = 3,
+            Down = 4,
+            UpperDiagonalLeft = 5,
+            UpperDiagonalRight = 6,
+            LowerDiagonalLeft = 7,
+            LowerDiagonalRight = 8
+        }
 
         [SerializeField]
         private Image backgroundImage = default;
+
+        // Control what direction the scrolling background should take
+        [SerializeField]
+        private ScrollDirection scrollDirection;
         // [SerializeField]
         private RectTransform backgroundContainer = default;
 
@@ -23,8 +39,16 @@ namespace CodeSampleModalLayer
             Vector2 offset;
             backgroundContainer = backgroundImage.GetComponent<RectTransform>();
 
+            if(scrollDirection == ScrollDirection.None)
+            {
+                // We don't want the background to scroll
+                return;
+            }
+
+
             // Offset is determined by the width of the background container
-            offset = new Vector2((backgroundContainer.rect.width*2), -(backgroundContainer.rect.height*2));
+            var offsetValues = GetOffsetBasedOnDirection();
+            offset = new Vector2(offsetValues.X, offsetValues.Y);
             
             // We need to create a copy of the material so we don't modify the 
             // actual material in the assets folder.
@@ -32,6 +56,35 @@ namespace CodeSampleModalLayer
             backgroundImage.material.DOOffset(offset, duration)
                 .SetLoops(-1, LoopType.Restart)
                 .SetEase(Ease.Linear);
+        }
+
+        public (float X, float Y) GetOffsetBasedOnDirection()
+        {
+            float backgroundWidth = backgroundContainer.rect.width;
+            float backgroundHeight = backgroundContainer.rect.height;
+
+            switch(scrollDirection)
+            {
+                case ScrollDirection.Left:
+                    return (backgroundWidth, 0);
+                case ScrollDirection.Right:
+                     return (-backgroundWidth, 0);
+                case ScrollDirection.Up:
+                    return (0f, -backgroundHeight );
+                case ScrollDirection.Down:
+                    return (0f,backgroundHeight );
+                case ScrollDirection.UpperDiagonalLeft:
+                    return (backgroundWidth, -backgroundHeight); 
+                case ScrollDirection.UpperDiagonalRight:
+                    return (-backgroundWidth, -backgroundHeight);
+                case ScrollDirection.LowerDiagonalLeft:
+                    return (backgroundWidth, backgroundHeight);
+                case ScrollDirection.LowerDiagonalRight:
+                    return (-backgroundWidth, backgroundHeight);
+                default:
+                    Debug.Log("ScrollingBackground GetOffsetBasedOnDirection():: No direction selected. Background will not scroll.");
+                    return (0,0);
+            }
         }
     }
 }
