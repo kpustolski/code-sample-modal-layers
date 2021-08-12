07ef7f6ff25870ca4aa964811b3f690d04920b1a
Katie Pustolski <pustolskik@gmail.com>
2021-08-11 19:55:31 -0700

    Created square item parent prefab and script.

---
+++ ModalLayeringSample/Assets/_Scripts/SquareItemParent.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CodeSampleModalLayer
+{
+    public class SquareItemParent : MonoBehaviour
+    {
+        [SerializeField]
+        private string id = default;
+
+
+        public void Setup()
+        {
+
+        }
+
+        public void Show()
+        {
+
+        }
+
+        public void Hide()
+        {
+
+        }
+
+        public void Shutdown()
+        {
+
+        }
+    }
+}
