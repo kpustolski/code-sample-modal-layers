d1bc33ebf78e674dbad672ea73054cd909162cb9
Katie Pustolski <pustolskik@gmail.com>
2021-08-14 13:00:54 -0700

    Created data manager class. Moved data related code from app manager to data manager class.

--- ModalLayeringSample/Assets/_Scripts/AppManager.cs
+++ ModalLayeringSample/Assets/_Scripts/AppManager.cs
@@ -23,15 +23,13 @@ namespace CodeSampleModalLayer
         private UIManager uIManager = default;
 
         public UIManager UIMan { get { return uIManager; } }
+        public DataManager DataMan { get { return dataManager; } private set { dataManager = value; } }
         public AppData AppDataObject { get { return appDataObject; } }
         // Global Static Variable
         public static AppManager Instance { get; private set; }
         public Backpack PlayerBackpack { get { return playerBackpack; } }
-        // Holds sorted item data by category
-        public Dictionary<Utilities.InventoryCategories, List<Item>> sortedItemData = new Dictionary<Utilities.InventoryCategories, List<Item>>();
 
-        // Holds unsorted item data
-        private ItemData itemData = default;
+        private DataManager dataManager = default;
         private Backpack playerBackpack = default;
         // Amount to decrease or increase in the inventory when we add or remove an item to the backpack
         private const int itemAmountDifference = 1;
@@ -39,69 +37,13 @@ namespace CodeSampleModalLayer
         // App Starts here. Ie. the "main" function
         void Start()
         {
-            itemData = new ItemData();
+            DataMan = new DataManager();
             playerBackpack = new Backpack();
             UIMan.Initialize();
+            DataMan.Initialize(jsonFile: AppDataObject.ItemJSONFile);
 
             Instance = this;
-            GetItemData();
-            SortItemListByCategory();
             homeView.Setup();
-        }
-
-        public void GetItemData()
-        {
-            TextAsset file = AppDataObject.ItemJSONFile;
-            if (file == null)
-            {
-                Debug.LogError($"AppManager.cs GetItemData() :: Unable to load the Item Data File.");
-                return;
-            }
-
-            // Deserialize the JSON from ItemData.JSON and store it in ItemData
-            Debug.Log($"File text: {file.text}");
-            JsonUtility.FromJsonOverwrite(file.text, itemData);
-            Debug.Log($"ItemData: {itemData.data.Count}");
-        }
-
-        private void SortItemListByCategory()
-        {
-            // Loop through each entry in the Utilities.InventoryCategories enum and sort the items
-            foreach (Utilities.InventoryCategories category in Enum.GetValues(typeof(Utilities.InventoryCategories)))
-            {
-                List<Item> tempList = new List<Item>();
-
-                if (category == Utilities.InventoryCategories.None)
-                {
-                    continue;
-                }
-
-                if (category == Utilities.InventoryCategories.All)
-                {
-                    sortedItemData.Add(category, itemData.data);
-                }
-                else
-                {
-                    foreach (Item i in itemData.data)
-                    {
-                        if (i.category != category)
-                        {
-                            continue;
-                        }
-
-                        tempList.Add(i);
-                    }
-                }
-
-                if (tempList.Count != 0)
-                {
-                    sortedItemData.Add(category, tempList);
-                }
-                else
-                {
-                    Debug.Log($"AppManager.cs SortItemListByCategory() :: No item of category {category} found. Did not add new object to sortedItemData.");
-                }
-            }
         }
 
         public void AddItemToBackpack(Item item)
---
+++ ModalLayeringSample/Assets/_Scripts/DataManager.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace CodeSampleModalLayer
+{
+    public class DataManager
+    {
+        // Holds unsorted item data
+        private ItemData itemData = default;
+        // Holds sorted item data by category
+        public Dictionary<Utilities.InventoryCategories, List<Item>> sortedItemData = new Dictionary<Utilities.InventoryCategories, List<Item>>();
+        private TextAsset mDataFile = default;
+
+        public void Initialize(TextAsset jsonFile)
+        {
+            itemData = new ItemData();
+            mDataFile = jsonFile;
+
+            GetItemData();
+            SortItemListByCategory();
+        }
+
+        public void GetItemData()
+        {
+            TextAsset file = mDataFile;
+            if (file == null)
+            {
+                Debug.LogError($"AppManager.cs GetItemData() :: Unable to load the Item Data File.");
+                return;
+            }
+
+            // Deserialize the JSON from ItemData.JSON and store it in ItemData
+            JsonUtility.FromJsonOverwrite(file.text, itemData);
+        }
+
+        private void SortItemListByCategory()
+        {
+            // Loop through each entry in the Utilities.InventoryCategories enum and sort the items
+            foreach (Utilities.InventoryCategories category in Enum.GetValues(typeof(Utilities.InventoryCategories)))
+            {
+                List<Item> tempList = new List<Item>();
+
+                if (category == Utilities.InventoryCategories.None)
+                {
+                    continue;
+                }
+
+                if (category == Utilities.InventoryCategories.All)
+                {
+                    sortedItemData.Add(category, itemData.data);
+                }
+                else
+                {
+                    foreach (Item i in itemData.data)
+                    {
+                        if (i.category != category)
+                        {
+                            continue;
+                        }
+
+                        tempList.Add(i);
+                    }
+                }
+
+                if (tempList.Count != 0)
+                {
+                    sortedItemData.Add(category, tempList);
+                }
+                else
+                {
+                    Debug.Log($"AppManager.cs SortItemListByCategory() :: No item of category {category} found. Did not add new object to sortedItemData.");
+                }
+            }
+        }
+    }
+}
