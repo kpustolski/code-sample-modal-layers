77d74550fb62bc2d5cc8887a636fca957d6c58bc
Katie Pustolski <pustolskik@gmail.com>
2021-08-16 21:14:13 -0700

    Updated backpack modal
    * Added space left text
    * Added backpack icon to the title
    * Adjusted the spacing of the grid in the scroll rect
    * Removed the tab content parent object and made the grid component on the content object of the scroll rect instead.
    * Added const variables for the strings.
    * Added a way to see if an item was removed from the backpack, prompting the UI and space text to update

--- ModalLayeringSample/Assets/_Scripts/BackpackModal.cs
+++ ModalLayeringSample/Assets/_Scripts/BackpackModal.cs
@@ -11,14 +11,20 @@ namespace CodeSampleModalLayer
         [Header("BackpackModal variables")]
         [Space(5)]
         [SerializeField]
+        private TextMeshProUGUI countText = default;
+        [SerializeField]
         private Button closeButton = default;
         [SerializeField]
-        private TextMeshProUGUI descriptionText = default;
+        private TextMeshProUGUI titleText = default;
         [SerializeField]
         private RectTransform itemParentTransform = default;
 
         private string modalId = default;
         private List<SquareItem> squareItemsList = new List<SquareItem>();
+        private const string kModalTitle = "My Backpack";
+        private const string kCountString = "Space Left: {0}/{1}";
+        // Stores the number of items in the backpack. It helps determine if we need to reset the modal UI.
+        private int currentItemCount = default;
 
 		public override void Initialize()
 		{
@@ -43,8 +49,9 @@ namespace CodeSampleModalLayer
             // Add to the modal layer list
             appMan.UIMan.AddToModalLayerList(this as IModalLayer);
 
-            descriptionText.text = "My Backpack";
+            titleText.text = kModalTitle;
             closeButton.onClick.AddListener(Shutdown);
+            currentItemCount = appMan.GetTotalItemsInBackpack();
             CreateBackpackContents();
         }
 
@@ -63,8 +70,19 @@ namespace CodeSampleModalLayer
         {
             ClearSquareItemsList();
             CreateBackpackContents();
+            currentItemCount = appMan.GetTotalItemsInBackpack();
         }
-
+        
+        //TODO: This is also used on the backpack button. Should I make the count text its own prefab and class?
+        public void UpdateCountText(int amount, int maxAmount)
+        {
+            countText.text = string.Format(kCountString, amount, maxAmount);
+            if(amount.Equals(maxAmount))
+            {
+                countText.text = "Full!";
+            }
+        }
+        
         private void CreateBackpackContents()
         {
             foreach (Item i in appMan.PlayerBackpack.ItemList)
@@ -73,6 +91,7 @@ namespace CodeSampleModalLayer
                 si.Setup(item: i, locationCreated: SquareItem.LocationCreated.backpackModal);
                 squareItemsList.Add(si);
             }
+            UpdateCountText(amount: appMan.PlayerBackpack.GetTotalItemsInBackpack(), maxAmount: appMan.PlayerBackpack.MaxTotalItems);
         }
 
         private void ClearSquareItemsList()
@@ -89,7 +108,12 @@ namespace CodeSampleModalLayer
 
         public void ShowLayer()
         {
-            Reset();
+            // If the currentItemCount variable does not match the current number of items in the backpack,
+            // then we know that the player removed an item. We need to update the UI to reflect the change.
+            if(currentItemCount != appMan.GetTotalItemsInBackpack())
+            {
+                Reset();
+            }
             this.gameObject.SetActive(true);
         }
         public void HideLayer()
